957ba1c81c30e977eed9322b765aec24eff197e2
klemengit <zaletelj.klemen@gmail.com>
2025-03-14 11:02:43 +0100

    fixed tests (np.alltrue --> np.all)

--- tests/test_tools/test_MCF.py
+++ tests/test_tools/test_MCF.py
@@ -8,14 +8,14 @@ def test_MCF():
     Test Modal Complexity Factor.
     """
 
-    assert np.alltrue(pyEMA.MCF(np.random.rand(5, 5) - 0.5) == 0)
-    assert np.alltrue(pyEMA.MCF((np.random.rand(5, 5) - 0.5)*1j) == 0)
-    assert np.alltrue(pyEMA.MCF(np.random.rand(5, 5) -0.5 + (np.random.rand(5, 5) - 0.5)*1j) != 0)
+    assert np.all(pyEMA.MCF(np.random.rand(5, 5) - 0.5) == 0)
+    assert np.all(pyEMA.MCF((np.random.rand(5, 5) - 0.5)*1j) == 0)
+    assert np.all(pyEMA.MCF(np.random.rand(5, 5) -0.5 + (np.random.rand(5, 5) - 0.5)*1j) != 0)
 
     # Same angle for all components
     a = np.random.rand(10, 10) - 0.5
     mode = a + a*1j
-    assert np.alltrue(pyEMA.MCF(mode) == 0)
+    assert np.all(pyEMA.MCF(mode) == 0)
 
 
 def test_MCF_single_mode():
